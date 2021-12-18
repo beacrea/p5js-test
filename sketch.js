b5fd76f997cf218c2f839b44d3b2908dedd8fbde
Coty Beasley <hi@coty.email>
2021-12-18 02:28:54 -0600

    Now much denser with lots more columns

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-let canvasW = 2200;
-let canvasH = canvasW / 3;
+let canvasW = 3000;
+let canvasH = canvasW / 3.55;
 
 function preload() {
   // Ensure the .ttf or .otf font stored in the assets directory
   // is loaded before setup() and draw() are called
-  font = loadFont('fonts/SourceCodePro-ExtraLight.ttf');
+  fontLight = loadFont('fonts/SourceCodePro-ExtraLight.ttf');
+  fontBold = loadFont('fonts/SourceCodePro-Regular.ttf');
 }
 
 function setup() {
   createCanvas(canvasW, canvasH);
   noFill();
   noLoop();
-  textFont(font);
+  textFont(fontBold);
   textAlign(CENTER, CENTER);
   // frameRate(1);
 }
@@ -30,6 +31,15 @@ function posVariance(pos, gridsize) {
 
 function opacityVariance() {
   return random(0.2, 1);
+}
+
+function findCompliment(color) {
+  if (color + 180 >= 360) {
+    return ((color + 180) - 360);
+  }
+  else {
+    return color + 180;
+  }
 }
 
 function gridNormal(width, height) {
@@ -67,8 +77,8 @@ function gridNormal(width, height) {
 
 function gridSkewA(width, height) {
   let margin = 100;
-  let colNum = random(40, 80);
-  let rowNum = colNum / 5;
+  let colNum = random(50, 100);
+  let rowNum = random(colNum, 10);
   let drawHeight = height - (margin * 2);
   let drawWidth = width - (margin * 2);
   let colWidth = drawWidth / colNum;
@@ -80,49 +90,59 @@ function gridSkewA(width, height) {
   console.log('Column Height: ' + rowHeight);
 
   // Draw each element from left to right, up to down
-  let hueStart = 200;
-  let hueEnd = 240;
+  let hueStart = 0;
+  let hueEnd = 360;
   let hueInit = random(hueStart, hueEnd);
   let hue = hueInit;
-  let satStart = 50;
-  let satEnd = 100;
+  let satStart = 0;
+  let satEnd = 10;
   let satInit = random(satStart, satEnd);
   let sat = satInit;
   let gridTrans = 1;
 
   
   for (let i = 0; i < colNum; i++) {
+
+    let hueSet = hue;
+    let hueCompliment = findCompliment(hue);
+
     for (let i = 0; i < rowNum; i++) {
 
       gridTrans = random(0, (i/rowNum));
+      strokeWeight(random(1, (rowHeight/10)))
 
       // Draw grid
       colorMode(HSB, 360, 100, 100, 1)
-      stroke(hueInit, satInit, 50, gridTrans);
+      stroke(hueInit, 250, 100, gridTrans);
       rect(posX, posY, colWidth, rowHeight);
 
       // Draw circles
       let circleX = posVariance(posX, colWidth);
       let circleY = posVariance(posY, rowHeight);
+      let circleSize = random((rowHeight * 0.0), (rowHeight * 1));
       colorMode(HSB, 360, 100, 100);
       strokeWeight(1);
       fill(0, 0, 0, 1);
-      stroke(hue, sat, 100, random(0.4, 1));
-      circle(circleX, circleY, random((rowHeight * 0.20), (rowHeight * 1)));
-      fill(hue, sat, 100, 1);
-      textSize(random(10, 56));
-      text('+', circleX, circleY);
+      stroke(hueSet, sat, 100, random(0.4, 1));
+      circle(circleX, circleY, circleSize);
+
+      // Draw text
+      fill(hueCompliment, 50, 100, 1);
+      console.log('Hue: ' + hueSet + '\n' + 'Compliment: ' + hueCompliment);
+      noStroke();
+      textSize(random(circleSize * 0.5));
+      // text(':D', circleX, circleY - ((circleSize * 0.5)/6));
 
       // Draw points
       fill(0, 0, 0, 1);
       stroke(360, 0, 100);
+      strokeWeight(random(1, 4));
       point(posX + (colWidth/2), posY + (rowHeight/2));
-
-      hue += (hueEnd-hue) * (i/rowNum);
       posY += rowHeight;
+      hue = (hueEnd - hue);
     }
+    hue = hueSet;
     gridTrans = random(0, (i/colNum));
-    hue += (hueEnd-hue) * (i/colNum);
     posY = margin;
     posX += colWidth;
   }
